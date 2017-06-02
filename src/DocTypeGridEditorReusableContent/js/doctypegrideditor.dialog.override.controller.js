5893dc8ad3a4628d3131bde929b7021897c2e403
Kyle Weems <weemsquirrel@gmail.com>
2017-06-02 13:02:38 -0700

    Capture values for node to be copied.

--- src/DocTypeGridEditorReusableContent/js/doctypegrideditor.dialog.override.controller.js
+++ src/DocTypeGridEditorReusableContent/js/doctypegrideditor.dialog.override.controller.js
@@ -4,7 +4,29 @@ angular.module('umbraco').controller('Our.Umbraco.DTGERCE.Dialog',
         // inherit core delete controller
         angular.extend(this, $controller('Our.Umbraco.DocTypeGridEditor.Dialogs.DocTypeGridEditorDialog', { $scope: $scope }));
 
-        console.info('ovrriding with DTGERCE');
+        console.info('overriding with DTGERCE');
 
+        function waitForNode() {
+            if ($scope.node !== null) {
+                var value = {
+                    name: $scope.dialogOptions.editorName
+                };
+
+                for (var t = 0; t < $scope.node.tabs.length; t++) {
+                    var tab = $scope.node.tabs[t];
+                    for (var p = 0; p < tab.properties.length; p++) {
+                        var prop = tab.properties[p];
+                        if (typeof prop.value !== "function") {
+                            value[prop.alias] = prop.value;
+                        }
+                    }
+                }
+                console.info('value of node in $scope', value);
+            } else {
+                setTimeout(waitForNode, 100);
+            }
+        };
+
+        waitForNode();
 
     }]);
